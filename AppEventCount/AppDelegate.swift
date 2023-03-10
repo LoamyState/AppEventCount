945971b7b1115a994ca9d0f72939b29417ae64ca
Jane Madsen <janemadsen815@gmail.com>
2023-03-10 09:30:33 -0700

    Complete lab

--- AppEventCount/AppDelegate.swift
+++ AppEventCount/AppDelegate.swift
@@ -10,16 +10,19 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+var launchCount = 0
+var configurationForConnectingCount = 0
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        launchCount += 1
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        configurationForConnectingCount += 1
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
--- AppEventCount/ViewController.swift
+++ AppEventCount/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var didFinishLaunchingLabel: UILabel!
+    @IBOutlet var configurationForConnectingLabel: UILabel!
+    @IBOutlet var willConnectToLabel: UILabel!
+    @IBOutlet var didBecomeActiveLabel: UILabel!
+    @IBOutlet var willResignActiveLabel: UILabel!
+    @IBOutlet var willEnterForegroundLabel: UILabel!
+    @IBOutlet var didEnterBackgroundLabel: UILabel!
+    
+    var willConnectToCount = 0
+    var didBecomeActiveCount = 0
+    var willResignActiveCount = 0
+    var willEnterForegroundCount = 0
+    var didEnterBackgroundCount = 0
+    
+    var appDelegate = (UIApplication.shared.delegate as! AppDelegate)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func updateView() {
+        didFinishLaunchingLabel.text = "App launched \(appDelegate.launchCount) time(s)"
+        configurationForConnectingLabel.text = "Configuration connected \(appDelegate.configurationForConnectingCount) time(s)"
+        willConnectToLabel.text = "Will Connect To: \(willConnectToCount) time(s)"
+        didBecomeActiveLabel.text = "Did Become Active: \(didBecomeActiveCount) time(s)"
+        willResignActiveLabel.text = "Will Resign Active: \(willResignActiveCount) time(s)"
+        willEnterForegroundLabel.text = "Will Enter Foreground: \(willEnterForegroundCount) time(s)"
+        didEnterBackgroundLabel.text = "Did Enter Background: \(didEnterBackgroundCount) time(s)"
+    }
 
 }
 
